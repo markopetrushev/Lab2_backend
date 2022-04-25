cb8c3e760065187f0a722e18169960021642b3d3
Петрушев Марко <marko.petrushev@students.finki.ukim.mk>
2022-04-25 20:00:08 +0200

    new commit

--- src/main/java/com/example/laboratoriska_2/web/rest/BookRestController.java
+++ src/main/java/com/example/laboratoriska_2/web/rest/BookRestController.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://lab2-front.herokuapp.com")
 @RequestMapping("/api/books")
 public class BookRestController {
 
